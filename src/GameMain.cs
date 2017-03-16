f9eebbaa519594b023288a621813248b97c602e6
Mitchell Williamson <mitchell.williamson97@gmail.com>
2017-03-17 03:15:38 +1100

    Add base SwinGame files

---
+++ src/GameMain.cs
@@ -0,0 +1,28 @@
+using System;
+using SwinGameSDK;
+
+namespace Battleship
+{
+    public class GameMain
+    {
+        public static void Main()
+        {
+            SwinGame.OpenGraphicsWindow("Battleship", 800, 600);
+            SwinGame.ShowSwinGameSplashScreen();
+
+            //Run the game loop
+            while (false == SwinGame.WindowCloseRequested())
+            {
+                //Fetch the next batch of UI interaction
+                SwinGame.ProcessEvents();
+
+                //Clear the screen and draw the framerate
+                SwinGame.ClearScreen(Color.White);
+                SwinGame.DrawFramerate(0, 0);
+
+                //Draw onto the screen
+                SwinGame.RefreshScreen(60);
+            }
+        }
+    }
+}
